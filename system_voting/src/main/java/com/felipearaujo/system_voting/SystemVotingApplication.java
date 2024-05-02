730bb84526176ba705b1510b4e4b7bb22440e036
FelipeAraujo32 <felipecafsx@gmail.com>
2024-05-02 16:55:38 -0300

    init

---
+++ system_voting/src/main/java/com/felipearaujo/system_voting/SystemVotingApplication.java
@@ -0,0 +1,13 @@
+package com.felipearaujo.system_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemVotingApplication.class, args);
+	}
+
+}
---
+++ system_voting/src/test/java/com/felipearaujo/system_voting/SystemVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearaujo.system_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
